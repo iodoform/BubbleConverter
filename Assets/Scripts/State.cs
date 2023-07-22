4d9e5b5f240cb3e99a55da87b7ad279ce2fe9533
kamide788 <81789053+kamide788@users.noreply.github.com>
2023-07-22 16:00:13 +0900

    StateMachineとStateの基底クラス作成

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State:MonoBehaviour
+{
+    ///<summary>
+    ///前の状態から現在の状態に遷移してきたときの処理
+    ///</summary>
+    virtual public void OnEnter() 
+    {
+
+    }
+
+    ///<summary>
+    ///前の状態から現在の状態に遷移してきたときに開始されるコルーチン
+    ///</summary>
+    virtual public IEnumerator EnterRoutine() 
+    {
+        yield return null;
+    }
+
+    ///<summary>
+    ///現在の状態から次の状態へ遷移するときの処理
+    ///</summary>
+    virtual public void OnExit()
+    {
+
+    }
+
+    ///<summary>
+    ///現在の状態から次の状態へ遷移するときに開始されるコルーチン
+    ///</summary>
+    virtual public IEnumerator ExitRoutine()
+    {
+        yield return null;
+    }
+
+    ///<summary>
+    ///状態遷移中以外は毎フレーム呼ばれる
+    ///MonoBehaviourのUpdate関数とほぼ同じ扱いでOK
+    ///</summary>
+    virtual public void OnUpdate(float deltaTime)
+    {
+
+    }
+}
