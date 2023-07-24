717bd197a979605aa488565f5504840665133842
Chihaya Kamide <kamide.chihaya.r8@dc.tohoku.ac.jp>
2023-07-24 18:24:20 +0900

    SymbolTableにtransitionTableを追加

--- Assets/Editor/SymbolTable.cs
+++ Assets/Editor/SymbolTable.cs
@@ -6,10 +6,12 @@ public class SymbolTable
 {
     private List<string> triggerTable;
     private List<string> stateTable;
+    private List<Transition> transitionTable;
     public SymbolTable()
     {
         triggerTable = new List<string>();
         stateTable = new List<string>();
+        transitionTable = new List<Transition>();
     }
     public void define(Tokenizer.TokenType tokenType, string token)
     {
@@ -20,6 +22,48 @@ public class SymbolTable
         else if (tokenType == Tokenizer.TokenType.STATE && !stateTable.Contains(token))
         {
             triggerTable.Add(token);
+        }
+    }
+
+    public void registerTransition(string fromState, string toState, string trigger = null)
+    {
+        Transition transition = new Transition(fromState,toState,trigger);
+        ParameterComparer comparer = new ParameterComparer();
+        // 被りがないかチェックしてから登録
+        if(!transitionTable.Contains(transition, comparer))
+        {
+            transitionTable.Add(transition);
+        }
+    }
+    private class Transition
+    {
+        public string fromState;
+        public string toState;
+        public string trigger;
+        public Transition(string fromState, string toState, string trigger = null)
+        {
+            this.fromState = fromState;
+            this.toState = toState;
+            this.trigger = trigger;
+        }
+    }
+    // transitionの比較用のクラス
+    private class ParameterComparer : IEqualityComparer<Transition>
+    {
+
+        public bool Equals(Transition x, Transition y)
+        {
+            if( x.fromState ==y.fromState &&
+                x.toState == y.toState &&
+                x.trigger == y.trigger )
+            {
+                return true;
+            }
+            return false;
+        }
+        public int GetHashCode(Transition obj)
+        {
+            return obj.fromState.GetHashCode()^obj.toState.GetHashCode()^obj.trigger.GetHashCode();
         }
     }
 }
