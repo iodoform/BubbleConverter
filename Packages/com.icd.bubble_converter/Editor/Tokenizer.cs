df18250f4a1cbff6070247a04c5ebb69ffd1b25e
Chihaya Kamide <kamide.chihaya.r8@dc.tohoku.ac.jp>
2023-07-30 19:19:04 +0900

    Tokenizerを変更

--- Packages/com.icd.bubble_converter/Editor/Tokenizer.cs
+++ Packages/com.icd.bubble_converter/Editor/Tokenizer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Text.RegularExpressions;
 using System.Linq;
+using System;
 namespace BubbleConverter
 {
     public class Tokenizer
@@ -17,6 +18,7 @@ namespace BubbleConverter
         private string[] text;
         private int currentTextPosition = -1;
         private string currentToken;
+        private TokenType previousTokenType = TokenType.STATE;
         public Tokenizer(string text)
         {
             // コメント及び不要な部分を削除
@@ -31,13 +33,113 @@ namespace BubbleConverter
             // 長さが0の文字列と改行記号を除外
             this.text = this.text.Where(s => !string.IsNullOrEmpty(s)).ToArray();
             this.text = this.text.Where(s => !(s=="\n")).ToArray();
+            //テキストが文法通りか確認
+            if(!isGrammatical()) Debug.LogError("Incorrect syntax in .md file");
         }
         private bool isGrammatical()
         {
             for(int i = 0;i<text.Length;i++)
             {
-                
+                // 次に来るトークンが文法的に正しいか判定
+                if(tokenType(text[i],i) == TokenType.STATE)
+                {
+                    TokenType next;
+                    try
+                    {
+                        next = tokenType(text[i+1],i+1);
+                    }
+                    catch (System.IndexOutOfRangeException)
+                    {
+                        if(previousTokenType==TokenType.ARROW)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            Debug.LogError("State must always be defined before or after a transition");
+                            return false;
+                        }
+                    }
+                    if(previousTokenType==TokenType.STATE || previousTokenType == TokenType.TRIGGER)
+                    {
+                        if(next!=TokenType.ARROW)
+                        {
+                            Debug.LogError("State must always be defined before or after a transition");
+                            return false;
+                        }
+                    }
+                    else if (previousTokenType == TokenType.ARROW)
+                    {
+                        if(!(next==TokenType.STATE||next==TokenType.SYMBOL))
+                        {
+                            Debug.LogError("One state token can only describe a destination or a source of a transition.");
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        Debug.LogError("A description around a state token is incorrect. Syntax error or unsupported syntax is used.");
+                        return false;
+                    }
+                }
+
+                if(tokenType(text[i],i) == TokenType.TRIGGER)
+                {
+                    TokenType next;
+                    try
+                    {
+                        next = tokenType(text[i+1],i+1);
+                    }
+                    catch (System.IndexOutOfRangeException)
+                    {
+                        return true;
+                    }
+                    if(next!=TokenType.STATE)
+                    {
+                        Debug.LogError("A trigger token should be written at a end of a line.");
+                        return false;
+                    }
+                }
+
+                if(tokenType(text[i],i) == TokenType.SYMBOL)
+                {
+                    TokenType next;
+                    try
+                    {
+                        next = tokenType(text[i+1],i+1);
+                    }
+                    catch (System.IndexOutOfRangeException)
+                    {
+                        Debug.LogError(("Always write a trigger after ':'."));
+                        return false;
+                    }
+                    if(next!=TokenType.TRIGGER)
+                    {
+                        Debug.LogError(("Always write a trigger after ':'."));
+                        return false;
+                    }
+                }
+                if(tokenType(text[i],i) == TokenType.ARROW)
+                {
+                    TokenType next;
+                    try
+                    {
+                        next = tokenType(text[i+1],i+1);
+                    }
+                    catch (System.IndexOutOfRangeException)
+                    {
+                        Debug.LogError(("Always write a state after '-->'."));
+                        return false;
+                    }
+                    if(next!=TokenType.STATE)
+                    {
+                        Debug.LogError(("Always write a state after '-->'."));
+                        return false;
+                    }
+                }
+                previousTokenType = tokenType(text[i],i);
             }
+            return false;
         }
         public bool hasMoreTokens()
         {
@@ -48,30 +150,50 @@ namespace BubbleConverter
             currentTextPosition+=1;
             currentToken = text[currentTextPosition];
         }
+
         public TokenType tokenType()
         {
+            return tokenType(currentToken, currentTextPosition);
+        }
+
+        public TokenType tokenType(string currentToken, int currentTextPosition)
+        {
             // SYMBOLか判定
-            if(currentToken=="\n"|currentToken==":")
+            if(currentToken==":")
             {
                 return TokenType.SYMBOL;
             }
-            if(currentToken=="-->")
+            if(currentToken == "-->")
             {
                 return TokenType.ARROW;
             }
-            // TRIGGERか判定
-            try
+            // TRIGGERかSTATEか判定
+            if(Regex.IsMatch(currentToken,@"\A(\w*)\Z"))
             {
-                if(text[currentTextPosition-1]==":")
+                try
                 {
-                    return TokenType.TRIGGER;
+                    if(text[currentTextPosition - 1]==":")
+                    {
+                        return TokenType.TRIGGER;
+                    }
+                    else
+                    {
+                        return TokenType.STATE;
+                    }
+                }
+                catch (System.IndexOutOfRangeException)
+                {
+                    return TokenType.STATE;
                 }
             }
-            catch (System.IndexOutOfRangeException)
+            else if(currentToken=="[*]")
             {
                 return TokenType.STATE;
             }
-            return TokenType.STATE;
+            else
+            {
+                throw new Exception("Triggers and States must be written in single-byte alphanumeric characters and underscores.");
+            }
         }
 
         public TokenType nextTokenType(int offset)
@@ -85,29 +207,7 @@ namespace BubbleConverter
             {
                 nextToken = null;
             }
-            
-            // SYMBOLか判定
-            if(nextToken==":")
-            {
-                return TokenType.SYMBOL;
-            }
-            if(nextToken=="-->")
-            {
-                return TokenType.ARROW;
-            }
-            // TRIGGERか判定
-            try
-            {
-                if(text[currentTextPosition+offset-1]==":")
-                {
-                    return TokenType.TRIGGER;
-                }
-            }
-            catch (System.IndexOutOfRangeException)
-            {
-                return TokenType.STATE;
-            }
-            return TokenType.STATE;
+            return tokenType(nextToken,currentTextPosition+offset);
         }
         public string token()
         {
